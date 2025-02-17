de085fd5130942a939516481d129043047c0690f
Khai <65278376+asiangoldfish@users.noreply.github.com>
2025-02-17 20:40:37 +0100

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      platformer-game = (with pkgs; stdenv.mkDerivation {
+          dontpatchshebangs = false;
+          pname = "3d-slicer";
+          version = "0.0.1";
+          src = fetchgit {
+            url = "https://github.com/Slicer/Slicer";
+            rev = "v5.8.0";
+            sha256 = "sha256-u8Z7/4j7/M/sSI+HQAd0FLOJCNcBxQF9ftzKOcCKiSM=";
+            fetchSubmodules = true;
+          };
+
+          env = {
+            CMAKE_TLS_VERIFY = 0;
+            EP_EXECUTE_DISABLE_CAPTURE_OUTPUTS = 1;
+          };
+
+          buildInputs = [
+            qt5.full
+            xorg.libXt
+      git
+            openssl
+            perl
+coreutils
+          ];
+
+          nativeBuildInputs = [
+            gcc
+            cmake
+          ];
+
+          buildPhase = ''
+            cd ..
+            sed '1c\#!/usr/bin/bash' Utilities/SetupForDevelopment.sh > Utilities/SetupForDevelopment.sh
+            mkdir Slicer-SuperBuild-Debug && cd Slicer-SuperBuild-Debug
+            cmake .. # -DCMAKE_BUILD_TYPE:STRING=Release ..
+            make
+          '';
+          installPhase = ''
+            mv Slicer-build/Slicer $out/bin
+          '';
+
+    # Specify content hash of fixed derivation output
+          outputHashAlgo = "sha256";
+          outputHashMode = "recursive";
+          outputHash = "";
+        }
+      );
+    in rec {
+      defaultApp = flake-utils.lib.mkApp {
+        drv = defaultPackage;
+      };
+      defaultPackage = platformer-game;
+      devShell = pkgs.mkShell {
+  buildInputs = [
+         platformer-game
+        ];
+      };
+    }
+  );
+}
+
