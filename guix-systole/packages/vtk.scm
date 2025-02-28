abcb659372ec23929937c6bd26df6a43e67c0036
Rafael Palomar <rafael.palomar@ous-research.no>
2025-02-28 14:51:03 +0100

    [STYLE][packages/vtk] Apply style to `vtk.scm` file

--- guix-systole/packages/vtk.scm
+++ guix-systole/packages/vtk.scm
@@ -1,12 +1,14 @@
 (define-module (guix-systole packages vtk)
-               #:use-module ((guix licenses) #:prefix license:);)
-               #:use-module (guix packages)
-               #:use-module (guix gexp)
-               #:use-module (guix download)
-               #:use-module (gnu packages qt)
-               #:use-module (ice-9 match)
-               #:use-module (srfi srfi-1)
-               #:use-module ((gnu packages image-processing) #:prefix imgproc:))
+  #:use-module ((guix licenses)
+                #:prefix license:) ;)
+  #:use-module (guix packages)
+  #:use-module (guix gexp)
+  #:use-module (guix download)
+  #:use-module (gnu packages qt)
+  #:use-module (ice-9 match)
+  #:use-module (srfi srfi-1)
+  #:use-module ((gnu packages image-processing)
+                #:prefix imgproc:))
 
 (define-public vtk
   (package
@@ -15,16 +17,16 @@
     (source
      (origin
        (method url-fetch)
-       (uri
-        "https://github.com/Slicer/VTK/archive/57bacae7e9d8613c1a5fc955028211c455f3787a.tar.gz")
+       (uri (string-append "https://github.com"
+                           "/Slicer/VTK/archive/refs/heads/"
+                           "slicer-v9.2.20230607-1ff325c54-2.zip"))
        (sha256
         (base32 "15f3ryx945p7d95asic9gnpz8jmais2gv1gh6vfcxm7malsl8m85"))))
     (arguments
      (list
       #:build-type "Release"
       #:configure-flags
-      #~'("-DBUILD_TESTING:BOOL=OFF"
-          "-DVTK_USE_EXTERNAL:BOOL=OFF"
+      #~'("-DBUILD_TESTING:BOOL=OFF" "-DVTK_USE_EXTERNAL:BOOL=OFF"
           "-DVTK_MODULE_USE_EXTERNAL_VTK_doubleconversion:BOOL=ON"
           "-DVTK_MODULE_USE_EXTERNAL_VTK_eigen:BOOL=ON"
           "-DVTK_MODULE_USE_EXTERNAL_VTK_expat:BOOL=ON"
@@ -59,7 +61,8 @@
           "-DVTK_QT_VERSION:STRING=5"
           "-DVTK_Group_Qt:BOOL=ON"
           "-DVTK_BUILD_TESTING:BOOL=OFF"
-          "-DCMAKE_INSTALL_LIBDIR:STRING=lib" ;Force value to prevent lib64 from being used on Linux
+          ;Force value to prevent lib64 from being used on Linux
+          "-DCMAKE_INSTALL_LIBDIR:STRING=lib"
           "-DVTK_MODULE_ENABLE_VTK_GUISupportQtQuick:STRING=NO"
           "-DVTK_LEGACY_REMOVE:BOOL=ON"
           "-DVTK_Group_Qt:BOOL=ON"
