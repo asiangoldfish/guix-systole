72916b9f3d0ee1d28b8ee3c1cacdec2ee1e6bf44
Khai Duong <duongkhai@gmail.com>
2025-03-14 12:56:45 +0100

    [ENH][packages/ctk] Add ctkapplauncher-slicer package definition
    [BUG][packages/ctk] Fix CTKAppLauncher name

--- guix-systole/packages/ctk.scm
+++ guix-systole/packages/ctk.scm
@@ -122,3 +122,30 @@ mutual interest and needs of the CTK community. The main scope of current CTK
 efforts includes the topics DICOM, DICOM Application Hosting, Widgets, and
 Plugin Framework.")
     (license license:asl2.0)))
+
+(define-public ctkapplauncher-slicer
+  (package
+    (name "ctkapplauncher")
+    (version "0.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri
+        "https://github.com/commontk/AppLauncher/archive/8759e03985738b8a8f3eb74ab516ba4e8ef29988.tar.gz")
+       (sha256
+        (base32 "1lrrcg9s39n357z2dhfhv8ff99biivdnwwxaggwvnpv9knppaz83"))))
+    (build-system cmake-build-system)
+    (arguments
+     '(#:tests? #f
+       #:configure-flags (list "-DBUILD_TESTING=OFF"
+                               "-DCTKAppLauncher_QT_VERSION=5"
+                               "-DCTKAppLauncher_INSTALL_LauncherLibrary=ON"
+                               ;; "-DCTK_INSTALL_LIB_DIR=lib64"
+                               )))
+    (inputs (list qtbase-5))
+    (home-page "http://www.commontk.org/")
+    (synopsis
+     "Simple and small program allowing to set the environment of any executable.")
+    (description
+     "Simple and small program allowing to set the environment of any executable.")
+    (license license:asl2.0)))
