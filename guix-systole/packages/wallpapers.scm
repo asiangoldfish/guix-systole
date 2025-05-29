7e71d6a1279d351c1ea9e72a4e5aa5d6f94f8956
aadne mauno <aadneom@aadneom-laptop.aadneom>
2025-05-30 01:06:21 +0200

    [STYLE][licenses] Add GPLv3 license notice
    [STYLE][packages] Add GPLv3 license notice
    
    [STYLE][packages] Add GPLv3 license notice
    
    [DOC][none] Rename GPLv3 license file
    
    [STYLE][licenses] Add copyright to license notice

--- guix-systole/packages/wallpapers.scm
+++ guix-systole/packages/wallpapers.scm
@@ -1,3 +1,18 @@
+;; 
+;; This file is part of SystoleOS.
+;;
+;; SystoleOS is free software: you can redistribute it and/or modify it under the 
+;; terms of the GNU General Public License as published by the Free Software 
+;; Foundation, either version 3 of the License, or (at your option) any later version.
+;;
+;; SystoleOS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+;; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+;; PURPOSE. See the GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License along 
+;; with SystoleOS. If not, see <https://www.gnu.org/licenses/>.
+;; 
+
 (define-module (guix-systole packages wallpapers)
   #:use-module (guix packages)
   #:use-module (guix download)
